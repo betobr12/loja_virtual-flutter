9d3b3f083700c857563b15fc760209e71fdc521e
dinari-roberto-carlos <roberto.carlos@dinaribank.com.br>
2020-11-13 17:58:59 -0300

    update

---
+++ lib/models/user_model.dart
@@ -0,0 +1,20 @@
+import 'package:scoped_model/scoped_model.dart';
+
+class UserModel extends Model{
+
+  bool isLoading = false;
+
+  void signUp(){
+
+  }
+  void signIn(){
+
+  }
+  void recoverPass(){
+
+  }
+
+  bool isLoggedIn(){
+
+  }
+}
